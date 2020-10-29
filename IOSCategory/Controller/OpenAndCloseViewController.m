1bf166655ed7b879e003b1f41f080da112caa2a6
1543044554.com <1543044554@qq.com>
2020-10-29 15:00:50 +0800

    修改

--- IOSCategory/Controller/OpenAndCloseViewController.m
+++ IOSCategory/Controller/OpenAndCloseViewController.m
@@ -7,6 +7,8 @@
 
 #import "OpenAndCloseViewController.h"
 
+#import "NSString+OpenAndClose.h"
+
 @interface OpenAndCloseViewController ()
 
 @end
@@ -16,16 +18,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    UILabel *lbl = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, 200, 0)];
+    lbl.font = [UIFont systemFontOfSize:16];
+    lbl.textColor = [UIColor blackColor];
+    lbl.backgroundColor = [UIColor redColor];
+    lbl.numberOfLines = 0;
+    [self.view addSubview:lbl];
+ 
+    //关闭
+    lbl.attributedText = [@"测试测试测试测试测试测试测试测试测\n试测test试测试测试测试测试测试测试test测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测test试测试" getAttributeStrWithLblWidth:200 font:[UIFont systemFontOfSize:16] lineSpacing:5 paragraphSpacing:10 lineBreakMode:NSLineBreakByWordWrapping closeLineNum:3 openState:NO];
+    //展开
+    lbl.attributedText = [@"测试测试测试测试测试测试测试测试测\n试测test试测试测试测试测试测试测试test测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测test试测试" getAttributeStrWithLblWidth:200 font:[UIFont systemFontOfSize:16] lineSpacing:5 paragraphSpacing:10 lineBreakMode:NSLineBreakByWordWrapping closeLineNum:3 openState:YES];
+    
+    //调整frame
+    [lbl sizeToFit];
+    
+    lbl.center = self.view.center;
+
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
