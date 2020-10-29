68311414ff7bc2df05fa3b8eb3470aaad03997e4
1543044554.com <1543044554@qq.com>
2020-10-29 11:46:04 +0800

    第一次

---
+++ IOSCategory/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IOSCategory
+//
+//  Created by Jeff on 2020/10/29.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (strong, nonatomic) UIWindow *window;
+@property(nonatomic,strong)UINavigationController *nav;
+
+@end
+
---
+++ IOSCategory/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  IOSCategory
+//
+//  Created by Jeff on 2020/10/29.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
